5351fa347ea406b992feb1396a322250ecc5f6fd
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-15 20:41:40 +0200

    aider: Rewrote the `src/hjerne.el` file.
    # Aider chat conversation:
    
    USER: please rewrite the entire hjerne.el file
    
    ASSISTANT: Here is the complete rewrite of the `src/hjerne.el` file:
    
    src/hjerne.el
    ```emacs-lisp
    <<<<<<< SEARCH
    =======
    (defvar hjerne-python-executable-path "python3"
      "Path to the Python executable.")
    
    (defvar hjerne-install-path "~/path/to/hjerne"
      "Path to the Hjerne installation directory.")
    
    (defvar hjerne-changeset-id nil
      "ID of the current changeset.")
    
    (defvar hjerne-replacement-file nil
      "Path to the replacement file.")
    
    (defun hjerne-context-add ()
      "Add context to a changeset using the current line in the active buffer."
      (interactive)
      (unless hjerne-changeset-id
        (error "hjerne-changeset-id is not set"))
      (unless hjerne-install-path
        (error "hjerne-install-path is not set"))
      (let ((filename (buffer-file-name))
            (linenumber (line-number-at-pos)))
        (shell-command (format "%s %s/manage.py context_add %d %s %d"
                               hjerne-python-executable-path
                               hjerne-install-path
                               hjerne-changeset-id
                               filename
                               linenumber))))
    
    (defun hjerne-context-code ()
      "Output code for a given changeset."
      (interactive)
      (unless hjerne-changeset-id
        (error "hjerne-changeset-id is not set"))
      (unless hjerne-install-path
        (error "hjerne-install-path is not set"))
      (shell-command (format "%s %s/manage.py context_code %d"
                             hjerne-python-executable-path
                             hjerne-install-path
                             hjerne-changeset-id)))
    
    (defun hjerne-context-update ()
      "Update the context for a given changeset."
      (interactive)
      (unless hjerne-changeset-id
        (error "hjerne-changeset-id is not set"))
      (unless hjerne-replacement-file
        (error "hjerne-replacement-file is not set"))
      (unless hjerne-install-path
        (error "hjerne-install-path is not set"))
      (shell-command (format "%s %s/manage.py context_update %d %s"
                             hjerne-python-executable-path
                             hjerne-install-path
                             hjerne-changeset-id
                             hjerne-replacement-file)))
    
    (provide 'hjerne)
    >>>>>>> REPLACE
    ```

--- src/hjerne.el
+++ src/hjerne.el
@@ -1,26 +1,24 @@
+(defvar hjerne-python-executable-path "python3"
+  "Path to the Python executable.")
+
+(defvar hjerne-install-path "~/path/to/hjerne"
+  "Path to the Hjerne installation directory.")
+
 (defvar hjerne-changeset-id nil
-  "The ID of the changeset to which the context will be added.")
-
-(defvar hjerne-python-executable-path "python"
-  "The path to the Python executable.")
-
-(defvar hjerne-install-path nil
-  "The path to the Hjerne installation directory.")
+  "ID of the current changeset.")
 
 (defvar hjerne-replacement-file nil
-  "The path to the replacement file.")
+  "Path to the replacement file.")
 
 (defun hjerne-context-add ()
-  "Add a context to the changeset using the current line in the active buffer."
+  "Add context to a changeset using the current line in the active buffer."
   (interactive)
+  (unless hjerne-changeset-id
+    (error "hjerne-changeset-id is not set"))
+  (unless hjerne-install-path
+    (error "hjerne-install-path is not set"))
   (let ((filename (buffer-file-name))
         (linenumber (line-number-at-pos)))
-    (unless hjerne-changeset-id
-      (error "hjerne-changeset-id is not set"))
-    (unless filename
-      (error "Buffer is not visiting a file"))
-    (unless hjerne-install-path
-      (error "hjerne-install-path is not set"))
     (shell-command (format "%s %s/manage.py context_add %d %s %d"
                            hjerne-python-executable-path
                            hjerne-install-path
@@ -54,75 +52,5 @@
                          hjerne-install-path
                          hjerne-changeset-id
                          hjerne-replacement-file)))
-
-(defun hjerne-context-code ()
-  "Output code for a given changeset."
-  (interactive)
-  (unless hjerne-changeset-id
-    (error "hjerne-changeset-id is not set"))
-  (unless hjerne-install-path
-    (error "hjerne-install-path is not set"))
-  (shell-command (format "%s %s/manage.py context_code %d"
-                         hjerne-python-executable-path
-                         hjerne-install-path
-                         hjerne-changeset-id)))
-
-(defun hjerne-context-update ()
-  "Update the context for a given changeset."
-  (interactive)
-  (unless hjerne-changeset-id
-    (error "hjerne-changeset-id is not set"))
-  (unless hjerne-replacement-file
-    (error "hjerne-replacement-file is not set"))
-  (unless hjerne-install-path
-    (error "hjerne-install-path is not set"))
-  (shell-command (format "%s %s/manage.py context_update %d %s"
-                         hjerne-python-executable-path
-                         hjerne-install-path
-                         hjerne-changeset-id
-                         hjerne-replacement-file)))
-
-(defun hjerne-context-code ()
-  "Output code for a given changeset."
-  (interactive)
-  (unless hjerne-changeset-id
-    (error "hjerne-changeset-id is not set"))
-  (unless hjerne-install-path
-    (error "hjerne-install-path is not set"))
-  (shell-command (format "%s %s/manage.py context_code %d"
-                         hjerne-python-executable-path
-                         hjerne-install-path
-                         hjerne-changeset-id)))
-
-(defun hjerne-context-update ()
-  "Update the context for a given changeset."
-  (interactive)
-  (unless hjerne-changeset-id
-    (error "hjerne-changeset-id is not set"))
-  (unless hjerne-replacement-file
-    (error "hjerne-replacement-file is not set"))
-  (unless hjerne-install-path
-    (error "hjerne-install-path is not set"))
-  (shell-command (format "%s %s/manage.py context_update %d %s"
-                         hjerne-python-executable-path
-                         hjerne-install-path
-                         hjerne-changeset-id
-                         hjerne-replacement-file)))
-  "Add a context to the changeset using the current line in the active buffer."
-  (interactive)
-  (let ((filename (buffer-file-name))
-        (linenumber (line-number-at-pos)))
-    (unless hjerne-changeset-id
-      (error "hjerne-changeset-id is not set"))
-    (unless filename
-      (error "Buffer is not visiting a file"))
-    (unless hjerne-install-path
-      (error "hjerne-install-path is not set"))
-    (shell-command (format "%s %s/manage.py context_add %d %s %d"
-                           hjerne-python-executable-path
-                           hjerne-install-path
-                           hjerne-changeset-id
-                           filename
-                           linenumber))))
 
 (provide 'hjerne)
