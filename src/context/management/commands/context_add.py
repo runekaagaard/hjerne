89622a161adc7787f4f2a2cf4061b141534e03d1
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-15 19:07:07 +0200

    Fixed the import path and updated symbol retrieval in context_add command.

--- src/context/management/commands/context_add.py
+++ src/context/management/commands/context_add.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from context.models import ChangeSet, Context
-from context.api import top_level_symbol_at  # Adjust the import path as necessary
+from context.api import top_level_symbol_at
 
 class Command(BaseCommand):
     help = 'Add a context to a changeset'
@@ -20,34 +20,7 @@ class Command(BaseCommand):
         except ChangeSet.DoesNotExist:
             raise CommandError('ChangeSet "%s" does not exist' % changeset_id)
 
-        symbol_name = top_level_symbol_at(filename, linenumber)
-        context = Context(change_set=changeset, file=filename, symbol=symbol_name)
-        context.save()
-
-        self.stdout.write(self.style.SUCCESS('Successfully added context to changeset "%s"' % changeset_id))
-from django.core.management.base import BaseCommand, CommandError
-from context.models import ChangeSet, Context
-from src.context.api import top_level_symbol_at  # Adjust the import path as necessary
-
-class Command(BaseCommand):
-    help = 'Add a context to a changeset'
-
-    def add_arguments(self, parser):
-        parser.add_argument('changeset_id', type=int, help='The ID of the changeset')
-        parser.add_argument('filename', type=str, help='The name of the file')
-        parser.add_argument('linenumber', type=int, help='The line number in the file')
-
-    def handle(self, *args, **kwargs):
-        changeset_id = kwargs['changeset_id']
-        filename = kwargs['filename']
-        linenumber = kwargs['linenumber']
-
-        try:
-            changeset = ChangeSet.objects.get(id=changeset_id)
-        except ChangeSet.DoesNotExist:
-            raise CommandError('ChangeSet "%s" does not exist' % changeset_id)
-
-        symbol_name = top_level_symbol_at(filename, linenumber)
+        symbol_name = top_level_symbol_at(filename, linenumber)["symbol_name"]
         context = Context(change_set=changeset, file=filename, symbol=symbol_name)
         context.save()
 
