9f23e2e9c7fb937b1c8f950106968ce7e841c474
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-15 22:50:17 +0200

    fixes

--- src/hjerne.el
+++ src/hjerne.el
@@ -156,4 +156,28 @@
          (selection (completing-read "Select changeset: " changesets)))
     (setq hjerne-changeset-id (string-to-number (car (split-string selection " "))))))
 
+(defun hjerne-get-current-git-branch ()
+  "Get the current git branch name."
+  (let ((branch (string-trim (shell-command-to-string "git rev-parse --abbrev-ref HEAD"))))
+    (unless (or (string= branch "xmain") (string= branch "xmaster"))
+      branch)))
+
+(defun hjerne-changeset-add ()
+  "Add a new changeset to the current project."
+  (interactive)
+  (unless hjerne-project-id
+    (error "hjerne-project-id is not set"))
+  (unless hjerne-install-path
+    (error "hjerne-install-path is not set"))
+  (let* ((default-title (hjerne-get-current-git-branch))
+         (title (read-string (if default-title
+                                 (format "Enter changeset title (default: %s): " default-title)
+                               "Enter changeset title: ")
+                             nil nil default-title)))
+    (shell-command (format "%s %s/manage.py changeset_add %d %s"
+                           hjerne-python-executable-path
+                           hjerne-install-path
+                           hjerne-project-id
+                           (shell-quote-argument title)))))
+
 (provide 'hjerne)
