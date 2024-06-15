350a2244b86ecbee7b17f40a6babf70bddccc5cf
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-15 20:00:17 +0200

    aider: Update the `context_update` command to accept `changeset_id` and `replacement_file` as arguments.
    # Aider chat conversation:
    
    USER: Its context_update [changeset_id] [replacement_file]
    
    ASSISTANT: To update the `context_update` command to accept `changeset_id` and `replacement_file` as arguments, we need to:
    
    1. Modify the `add_arguments` method to accept `replacement_file`.
    2. Update the `handle` method to use the `replacement_file` argument.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src/context/management/commands/context_update.py
    <source>python
    <<<<<<< SEARCH
            parser.add_argument('changeset_id', type=int, help='The ID of the changeset')
    =======
            parser.add_argument('changeset_id', type=int, help='The ID of the changeset')
            parser.add_argument('replacement_file', type=str, help='The path to the replacement file')
    >>>>>>> REPLACE
    </source>
    
    src/context/management/commands/context_update.py
    <source>python
    <<<<<<< SEARCH
            for context in contexts:
    =======
            replacement_file_path = kwargs['replacement_file']
    
            for context in contexts:
                source_file_path = context.file
                destination_file_path = context.file  # Update the same file
    
                replacement_code = code_for_context(context)
                update_symbol(source_file_path, context.symbol, replacement_code)
    >>>>>>> REPLACE
    </source>

--- src/context/management/commands/context_update.py
+++ src/context/management/commands/context_update.py
@@ -8,6 +8,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('changeset_id', type=int, help='The ID of the changeset')
+        parser.add_argument('replacement_file', type=str, help='The path to the replacement file')
 
     def handle(self, *args, **kwargs):
         changeset_id = kwargs['changeset_id']
@@ -18,7 +19,14 @@ class Command(BaseCommand):
             raise CommandError('ChangeSet "%s" does not exist' % changeset_id)
 
         contexts = Context.objects.filter(change_set=changeset)
+        replacement_file_path = kwargs['replacement_file']
+        
         for context in contexts:
+            source_file_path = context.file
+            destination_file_path = context.file  # Update the same file
+
+            replacement_code = code_for_context(context)
+            update_symbol(source_file_path, context.symbol, replacement_code)
             source_file_path = context.file
             replacement_file_path = context.file  # Assuming the replacement code is in the same file
             destination_file_path = context.file  # Update the same file
