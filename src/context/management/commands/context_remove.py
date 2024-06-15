5e9d04dd71ebc888ae69481f6058fb85b4a5e27c
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-15 22:18:01 +0200

    aider: Added management command `context_remove` to remove a context by `changeset_id` and `symbol_name`.
    # Aider chat conversation:
    
    USER: I did `git reset --hard HEAD~1` to discard the last edits. Please wait for further instructions before attempting that change again. Feel free to ask relevant questions about why the changes were reverted.
    
    ASSISTANT: Understood. If you could provide more context on why the changes were reverted, it would help me better understand your requirements and make more appropriate suggestions. What would you like to do next?
    
    USER: Just output the function here, I'll paste it in.
    
    ASSISTANT: Here is the function you requested:
    
    ```python
    def parse_markdown(source_code):
        """
        Extract all ```python ... ``` blocks and join them with an empty line between them.
        """
        import re
        pattern = re.compile(r'```python(.*?)```', re.DOTALL)
        matches = pattern.findall(source_code.decode())
        return "\n\n".join(matches).encode()
    ```
    
    USER: Now lets add a context_remove changeset_id symbol name management command
    
    ASSISTANT: To add a new management command `context_remove` that removes a context by `changeset_id` and `symbol_name`, we need to:
    
    1. Create a new file `src/context/management/commands/context_remove.py`.
    2. Implement the command to remove the specified context.
    
    Here is the *SEARCH/REPLACE* block for the new file:
    
    src/context/management/commands/context_remove.py
    <source>python
    <<<<<<< SEARCH
    =======
    from django.core.management.base import BaseCommand, CommandError
    from context.models import ChangeSet, Context
    
    class Command(BaseCommand):
        help = 'Remove a context from a changeset'
    
        def add_arguments(self, parser):
            parser.add_argument('changeset_id', type=int, help='The ID of the changeset')
            parser.add_argument('symbol_name', type=str, help='The name of the symbol to remove')
    
        def handle(self, *args, **kwargs):
            changeset_id = kwargs['changeset_id']
            symbol_name = kwargs['symbol_name']
    
            try:
                changeset = ChangeSet.objects.get(id=changeset_id)
            except ChangeSet.DoesNotExist:
                raise CommandError('ChangeSet "%s" does not exist' % changeset_id)
    
            try:
                context = Context.objects.get(change_set=changeset, symbol=symbol_name)
                context.delete()
                self.stdout.write(self.style.SUCCESS('Successfully removed context for symbol "%s" from changeset "%s"' % (symbol_name, changeset_id)))
            except Context.DoesNotExist:
                raise CommandError('Context for symbol "%s" does not exist in changeset "%s"' % (symbol_name, changeset_id))
    >>>>>>> REPLACE
    </source>

---
+++ src/context/management/commands/context_remove.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+from context.models import ChangeSet, Context
+
+class Command(BaseCommand):
+    help = 'Remove a context from a changeset'
+
+    def add_arguments(self, parser):
+        parser.add_argument('changeset_id', type=int, help='The ID of the changeset')
+        parser.add_argument('symbol_name', type=str, help='The name of the symbol to remove')
+
+    def handle(self, *args, **kwargs):
+        changeset_id = kwargs['changeset_id']
+        symbol_name = kwargs['symbol_name']
+
+        try:
+            changeset = ChangeSet.objects.get(id=changeset_id)
+        except ChangeSet.DoesNotExist:
+            raise CommandError('ChangeSet "%s" does not exist' % changeset_id)
+
+        try:
+            context = Context.objects.get(change_set=changeset, symbol=symbol_name)
+            context.delete()
+            self.stdout.write(self.style.SUCCESS('Successfully removed context for symbol "%s" from changeset "%s"' % (symbol_name, changeset_id)))
+        except Context.DoesNotExist:
+            raise CommandError('Context for symbol "%s" does not exist in changeset "%s"' % (symbol_name, changeset_id))
