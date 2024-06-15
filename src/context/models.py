0eac8fa5334b0e0595ea536f4bf93906b29fd0ea
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-15 18:42:27 +0200

    Added models to admin for registration.

---
+++ src/context/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Project
+    title
+    description
+
+class ChangeSet
+    project
+    title
+    description - optional
+
+class Context
+    change_set
+    file
+    symbol, textfield
+    
