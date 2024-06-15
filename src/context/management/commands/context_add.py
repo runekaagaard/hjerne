d778ce6983c90e11d7723f5e77be6360eb8586a1
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-15 19:00:42 +0200

    aider: Created a custom management command `context_add` to add context to a changeset.
    # Aider chat conversation:
    
    USER: try again.
    
    ASSISTANT: To create a custom management command `context_add` that takes `changeset_id`, `filename`, and `linenumber` as arguments, we need to:
    
    1. Create a new directory `management/commands` inside the `context` app.
    2. Add an `__init__.py` file in the `management` and `commands` directories.
    3. Create the `context_add.py` file with the command implementation.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src/context/management/__init__.py
    ```python
    <<<<<<< SEARCH
    # This file is required to make the management directory a Python package
    =======
    # This file is required to make the management directory a Python package
    >>>>>>> REPLACE
    ```
    
    src/context/management/commands/__init__.py
    ```python
    <<<<<<< SEARCH
    # This file is required to make the commands directory a Python package
    =======
    # This file is required to make the commands directory a Python package
    >>>>>>> REPLACE
    ```
    
    src/context/management/commands/context_add.py
    ```python
    <<<<<<< SEARCH
    =======
    from django.core.management.base import BaseCommand, CommandError
    from context.models import ChangeSet, Context
    from src.context.api import top_level_symbol_at  # Adjust the import path as necessary
    
    class Command(BaseCommand):
        help = 'Add a context to a changeset'
    
        def add_arguments(self, parser):
            parser.add_argument('changeset_id', type=int, help='The ID of the changeset')
            parser.add_argument('filename', type=str, help='The name of the file')
            parser.add_argument('linenumber', type=int, help='The line number in the file')
    
        def handle(self, *args, **kwargs):
            changeset_id = kwargs['changeset_id']
            filename = kwargs['filename']
            linenumber = kwargs['linenumber']
    
            try:
                changeset = ChangeSet.objects.get(id=changeset_id)
            except ChangeSet.DoesNotExist:
                raise CommandError('ChangeSet "%s" does not exist' % changeset_id)
    
            symbol_name = top_level_symbol_at(filename, linenumber)
            context = Context(change_set=changeset, file=filename, symbol=symbol_name)
            context.save()
    
            self.stdout.write(self.style.SUCCESS('Successfully added context to changeset "%s"' % changeset_id))
    >>>>>>> REPLACE
    ```

--- src/context/management/commands/context_add.py
+++ src/context/management/commands/context_add.py
@@ -25,3 +25,30 @@ class Command(BaseCommand):
         context.save()
 
         self.stdout.write(self.style.SUCCESS('Successfully added context to changeset "%s"' % changeset_id))
+from django.core.management.base import BaseCommand, CommandError
+from context.models import ChangeSet, Context
+from src.context.api import top_level_symbol_at  # Adjust the import path as necessary
+
+class Command(BaseCommand):
+    help = 'Add a context to a changeset'
+
+    def add_arguments(self, parser):
+        parser.add_argument('changeset_id', type=int, help='The ID of the changeset')
+        parser.add_argument('filename', type=str, help='The name of the file')
+        parser.add_argument('linenumber', type=int, help='The line number in the file')
+
+    def handle(self, *args, **kwargs):
+        changeset_id = kwargs['changeset_id']
+        filename = kwargs['filename']
+        linenumber = kwargs['linenumber']
+
+        try:
+            changeset = ChangeSet.objects.get(id=changeset_id)
+        except ChangeSet.DoesNotExist:
+            raise CommandError('ChangeSet "%s" does not exist' % changeset_id)
+
+        symbol_name = top_level_symbol_at(filename, linenumber)
+        context = Context(change_set=changeset, file=filename, symbol=symbol_name)
+        context.save()
+
+        self.stdout.write(self.style.SUCCESS('Successfully added context to changeset "%s"' % changeset_id))
