f50099c073dbced45bd5e7d18eea8e6adc82fa1c
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-15 22:05:54 +0200

    Added option to extract code from markdown blocks when updating context.

--- src/context/management/commands/context_update.py
+++ src/context/management/commands/context_update.py
@@ -9,9 +9,8 @@ class Command(BaseCommand):
         parser.add_argument('changeset_id', type=int, help='The ID of the changeset')
         parser.add_argument('replacement_file', type=str, help='The path to the replacement file')
 
-    def handle(self, *args, **kwargs):
+        parser.add_argument('--from-markdown', action='store_true', help='Extract code from markdown blocks')
         changeset_id = kwargs['changeset_id']
-        from_markdown = kwargs['from_markdown']
 
         try:
             changeset = ChangeSet.objects.get(id=changeset_id)
@@ -23,10 +22,14 @@ class Command(BaseCommand):
         for context in contexts:
             from context.api import init_file, extract_code_from_markdown
             _, tree, query = init_file(kwargs['replacement_file'])
-            replacement_symbols = {
+            if from_markdown:
+                from context.api import extract_code_from_markdown
+                replacement_symbols = extract_code_from_markdown(kwargs['replacement_file'])
+            else:
+                replacement_symbols = {
                 symbol['symbol_name']: symbol['node'].text.decode()
                 for symbol in top_level_symbols(tree, query, from_markdown=from_markdown)
-            }
+                }
 
             if context.symbol in replacement_symbols:
                 update_symbol(context.file, context.symbol, replacement_symbols[context.symbol])
