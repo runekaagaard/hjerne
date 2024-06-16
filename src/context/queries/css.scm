9ecaafcbb8d152e85d295f24c1c6cdfd1e9aeb5d
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-16 18:16:28 +0200

    aider: Add symbol.name to the current css.scm.
    # Aider chat conversation:
    
    USER: Add symbol.name to the current css.scm
    
    ASSISTANT: Here is the *SEARCH/REPLACE* block to add `symbol.name` to the current `src/context/queries/css.scm` file:
    
    src/context/queries/css.scm
    <source>scheme
    <<<<<<< SEARCH
    [
      (block)
      (declaration)
    ] @indent.begin
    
    (block
      "}" @indent.branch)
    
    "}" @indent.dedent
    
    (comment) @indent.ignore
    =======
    [
      (block)
      (declaration)
    ] @indent.begin
    
    (block
      "}" @indent.branch)
    
    "}" @indent.dedent
    
    (comment) @indent.ignore
    
    ; Match top-level rules
    (rule_set
      (qualified_rule
        (selector_list) @symbol.name))
    
    ; Match top-level at-rules
    (at_rule
      (at_keyword) @symbol.name)
    >>>>>>> REPLACE
    </source>

--- src/context/queries/css.scm
+++ src/context/queries/css.scm
@@ -9,3 +9,12 @@
 "}" @indent.dedent
 
 (comment) @indent.ignore
+
+; Match top-level rules
+(rule_set
+  (qualified_rule
+    (selector_list) @symbol.name))
+
+; Match top-level at-rules
+(at_rule
+  (at_keyword) @symbol.name)
