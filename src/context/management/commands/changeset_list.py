64f708a19e8de1111dd47e12764374db6a1ed873
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-16 12:55:30 +0200

    Added ability to filter changesets by project ID.

--- src/context/management/commands/changeset_list.py
+++ src/context/management/commands/changeset_list.py
@@ -1,10 +1,19 @@
 from django.core.management.base import BaseCommand
-from context.models import Changeset
+from context.models import Changeset, Project
 
 class Command(BaseCommand):
     help = 'List all changesets with their IDs and titles'
 
+    def add_arguments(self, parser):
+        parser.add_argument('project_id', type=int, help='The ID of the project')
+
     def handle(self, *args, **kwargs):
-        changesets = Changeset.objects.all()
+        project_id = kwargs['project_id']
+
+        try:
+            project = Project.objects.get(id=project_id)
+        except Project.DoesNotExist:
+            raise CommandError('Project "%s" does not exist' % project_id)
+        changesets = Changeset.objects.filter(project=project)
         for changeset in changesets:
             self.stdout.write(f"{changeset.id} {changeset.project.title}.{changeset.title}")
