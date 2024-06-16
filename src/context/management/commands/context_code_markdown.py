03b68097944b3cdb681228d61ddc3b3a0dc86d6c
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-16 20:36:22 +0200

    aider: Created a new management command `context_code_markdown.py` to output code in markdown format grouped by language.
    # Aider chat conversation:
    
    USER: Lets create a context_code_markdown that outputs on this format:
    
    # 3 Project title - 2 feature title
    feature description
    
    ## file: /home/r/ws/sag/src/psy/apps/help/pfa/core.py
    ```python
    def get_config_from_hypertrek_state(state):
        slug = state['hypertrek']['slug']
        return PFA_ONLINE_PRODUCT_SETTINGS_BY_SLUG[slug]
    ```
    
    ## file: /home/r/ws/sag/src/psy/apps/help/static/help/appointments.css
    ```css
    #appointments #content {
      padding-bottom: 20px;
    }
    
    #appointments #timeslot-booked {
      padding: 0 20px;
    }
    ```
    
    It should group the files by language.
    
    ASSISTANT: To create the `context_code_markdown` command, we need to:
    
    1. Create a new management command file `context_code_markdown.py`.
    2. Implement the logic to output the context code in the specified markdown format, grouped by language.
    
    Here are the changes:
    
    src/context/management/commands/context_code_markdown.py
    <source>python
    <<<<<<< SEARCH
    =======
    from django.core.management.base import BaseCommand, CommandError
    from context.models import Changeset, Context
    from context.api import code_for_context
    import mimetypes
    
    class Command(BaseCommand):
        help = 'Output code for a given changeset in markdown format, grouped by language'
    
        def add_arguments(self, parser):
            parser.add_argument('changeset_id', type=int, help='The ID of the changeset')
    
        def handle(self, *args, **kwargs):
            changeset_id = kwargs['changeset_id']
    
            try:
                changeset = Changeset.objects.get(id=changeset_id)
            except Changeset.DoesNotExist:
                raise CommandError('Changeset "%s" does not exist' % changeset_id)
    
            contexts = Context.objects.filter(changeset=changeset)
            contexts_by_file = {}
            for context in contexts:
                if context.file not in contexts_by_file:
                    contexts_by_file[context.file] = []
                contexts_by_file[context.file].append(context)
    
            self.stdout.write(f"# {changeset.project.id} {changeset.project.title} - {changeset.id} {changeset.title}\n")
            self.stdout.write(f"{changeset.description}\n")
    
            files_by_language = {}
            for file, contexts in contexts_by_file.items():
                language = mimetypes.guess_type(file)[0].split("/")[-1].split("-")[-1]
                if language not in files_by_language:
                    files_by_language[language] = []
                files_by_language[language].append((file, contexts))
    
            for language, files in files_by_language.items():
                for file, contexts in files:
                    self.stdout.write(f"\n## file: {file}\n")
                    self.stdout.write(f"```{language}\n")
                    for context in contexts:
                        code = code_for_context(context)
                        self.stdout.write(code)
                        self.stdout.write("\n")
                    self.stdout.write(f"```\n")
    >>>>>>> REPLACE
    </source>

---
+++ src/context/management/commands/context_code_markdown.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand, CommandError
+from context.models import Changeset, Context
+from context.api import code_for_context
+import mimetypes
+
+class Command(BaseCommand):
+    help = 'Output code for a given changeset in markdown format, grouped by language'
+
+    def add_arguments(self, parser):
+        parser.add_argument('changeset_id', type=int, help='The ID of the changeset')
+
+    def handle(self, *args, **kwargs):
+        changeset_id = kwargs['changeset_id']
+
+        try:
+            changeset = Changeset.objects.get(id=changeset_id)
+        except Changeset.DoesNotExist:
+            raise CommandError('Changeset "%s" does not exist' % changeset_id)
+
+        contexts = Context.objects.filter(changeset=changeset)
+        contexts_by_file = {}
+        for context in contexts:
+            if context.file not in contexts_by_file:
+                contexts_by_file[context.file] = []
+            contexts_by_file[context.file].append(context)
+
+        self.stdout.write(f"# {changeset.project.id} {changeset.project.title} - {changeset.id} {changeset.title}\n")
+        self.stdout.write(f"{changeset.description}\n")
+
+        files_by_language = {}
+        for file, contexts in contexts_by_file.items():
+            language = mimetypes.guess_type(file)[0].split("/")[-1].split("-")[-1]
+            if language not in files_by_language:
+                files_by_language[language] = []
+            files_by_language[language].append((file, contexts))
+
+        for language, files in files_by_language.items():
+            for file, contexts in files:
+                self.stdout.write(f"\n## file: {file}\n")
+                self.stdout.write(f"```{language}\n")
+                for context in contexts:
+                    code = code_for_context(context)
+                    self.stdout.write(code)
+                    self.stdout.write("\n")
+                self.stdout.write(f"```\n")
