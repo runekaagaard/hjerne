7034e41d6880485af176f66ccc06cd3d9bc77317
Rune Kaagaard <rumi.kg@gmail.com>
2024-06-17 21:18:45 +0200

    more jsx examples

--- src/context/queries/js.js
+++ src/context/queries/js.js
@@ -43,3 +43,112 @@ tArrow39_41 => (foo, bar) {
 export const exportFunction43_45 = function ({a, b}) {
   return 9
 }
+
+// Function Declaration
+function symbol1() {
+  console.log('Function Declaration');
+}
+
+// Function Expression
+var symbol2 = function() {
+  console.log('Function Expression');
+};
+
+// Function Expression using const
+const symbol3 = function() {
+  console.log('Function Expression with const');
+};
+
+// Arrow Function
+const symbol4 = () => {
+  console.log('Arrow Function');
+};
+
+// Variable Declaration (var, let, const)
+var symbol5 = 1;
+let symbol6 = 2;
+const symbol7 = 3;
+
+// Class Declaration
+class symbol8 {
+  constructor() {
+    console.log('Class Declaration');
+  }
+}
+
+// Object Property Assignment (ES6 shorthand)
+const symbo9 = {
+  symbol9() {
+    console.log('Object Method');
+  },
+  zzzz: 42
+};
+
+// Assignment Expression
+symbol10 = 10;
+
+// Export Declarations
+export function symbol11() {
+  console.log('Exported Function');
+}
+
+export class symbol12 {
+  constructor() {
+    console.log('Exported Class');
+  }
+}
+
+export const symbol13 = 42;
+export let symbol14 = 'exported let';
+export var symbol15 = true; // Not typically used but valid
+
+// Named Exports
+const symbol16 = function() {};
+export { symbol16 };
+
+// Default Export
+export default function symbol17() {
+  console.log('Default Export Function');
+}
+
+// Component Function Declaration
+function SymbolComponent1() {
+  return <div>Component 1</div>;
+}
+
+// Component Function Expression
+const SymbolComponent2 = function() {
+  return <div>Component 2</div>;
+};
+
+// Component Arrow Function
+const SymbolComponent3 = () => {
+  return <div>Component 3</div>;
+};
+
+// Class Component
+class SymbolComponent4 extends React.Component {
+  render() {
+    return <div>Component 4</div>;
+  }
+}
+
+// Export Declarations in JSX
+export function SymbolComponent5() {
+  return <div>Exported Component 1</div>;
+}
+
+export const SymbolComponent6 = () => {
+  return <div>Exported Component 2</div>;
+}
+
+export class SymbolComponent7 extends React.Component {
+  render() {
+    return <div>Exported Component 3</div>;
+  }
+}
+
+// Default Export in JSX
+export default function SymbolComponent8() {
+  return <div>Default Export Component</div>;
+}
